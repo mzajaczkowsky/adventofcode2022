0cb8537d35c76cf6bd341f5441f1b58d17ae0fe2
Maciej Zajaczkowski <mzajaczkowsky@gmail.com>
2022-12-03 07:48:27 +0100

    2nd December part two

--- day2.js
+++ day2.js
@@ -1,4 +1,4 @@
-// December 1st, 2022
+// December 2st, 2022
 const fs = require("fs");
 
 fs.readFile("file.txt", (err, data) => {
@@ -7,23 +7,20 @@ fs.readFile("file.txt", (err, data) => {
     
     const result = numbers.map(e =>{
         sum = 0;
-        if(e[2] === "X"){
-            sum += 1;
-            if(e[0] === "A") return sum += 3
-            if(e[0] === "B") return sum += 0
-            if(e[0] === "C") return sum += 6
+        if(e[0] === "A"){
+            if(e[2] === "X") return sum += 3    // +0 for lose and +3 for scissors
+            if(e[2] === "Y") return sum += 4    // +3 for draw and +1 for rock
+            if(e[2] === "Z") return sum += 8    // +6 for win and +2 for paper
         }
-        if(e[2] === "Y"){
-            sum += 2;
-            if(e[0] === "A") return sum += 6
-            if(e[0] === "B") return sum += 3
-            if(e[0] === "C") return sum += 0
+        if(e[0] === "B"){
+            if(e[2] === "X") return sum += 1    // +0 for lose and +1 for rock
+            if(e[2] === "Y") return sum += 5    // +3 for draw and +2 for paper
+            if(e[2] === "Z") return sum += 9    // +6 for win and +3 for scissors
         }
-        if(e[2] === "Z"){
-            sum += 3;
-            if(e[0] === "A") return sum += 0
-            if(e[0] === "B") return sum += 6
-            if(e[0] === "C") return sum += 3
+        if(e[0] === "C"){
+            if(e[2] === "X") return sum += 2    // +0 for lose and +2 for paper
+            if(e[2] === "Y") return sum += 6    // +3 for draw and +3 for scissors
+            if(e[2] === "Z") return sum += 7    // +6 for win and +1 for rock
         }
     })    
     let score = 0; 
