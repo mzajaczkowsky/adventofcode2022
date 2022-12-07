53e085d385d2f50330117adadcefb136a3d3c094
mzajaczkowsky <mzajaczkowsky@gmail.com>
2022-12-07 03:20:58 +0100

    6th December part two

---
+++ day6-2.js
@@ -0,0 +1,21 @@
+// December 6th, 2022
+const fs = require("fs");
+
+fs.readFile("file.txt", (err, data) => {
+    if (err) throw err;
+    const moves = data.toString().split('');
+    let arr = [];
+    let counter = 0;
+    moves.forEach(move =>{ 
+        if(arr.length > 13) return;
+        counter++;
+        if(arr.includes(move)) {
+            arr.splice(0, arr.indexOf(move)+1);            
+        }
+        if(!arr.includes(move)){
+            arr.push(move);
+        }        
+    })
+
+    console.log('Result: ' + counter)
+});
